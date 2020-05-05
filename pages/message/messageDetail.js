63dd1a9213e45930819b4a27e019abf775a06939
zjjaxx <2495893564@qq.com>
2020-05-05 15:36:55 +0800

    chat

--- pages/message/messageDetail.js
+++ pages/message/messageDetail.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from "react"
-import { View, Text, StyleSheet, SafeAreaView, } from "react-native"
+import { View, Text, StyleSheet, SafeAreaView,Button } from "react-native"
 import Header from "../../components/Header"
 import { GiftedChat } from 'react-native-gifted-chat'
 function MessageDetail({ navigation }) {
@@ -10,7 +10,7 @@ function MessageDetail({ navigation }) {
         user: {
             _id: 2,
             name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
+            avatar: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1703743334,411607732&fm=26&gp=0.jpg',
         },
     },])
     const onSend = useCallback((_messages = []) => {
@@ -21,11 +21,43 @@ function MessageDetail({ navigation }) {
     const leftEvent = useCallback(() => {
         navigation.goBack()
     }, [])
+    //接收消息receiveMessage
+    const receiveMessage=useCallback(()=>{
+        setMessages([  {
+            _id: messages.length+1,
+            text: 'This is a quick reply. Do you love Gifted Chat? (radio) KEEP IT',
+            createdAt: new Date(),
+            quickReplies: {
+              type: 'radio', // or 'checkbox',
+              keepIt: true,
+              values: [
+                {
+                  title: '😋 Yes',
+                  value: 'yes',
+                },
+                {
+                  title: '📷 Yes, let me show you with a picture!',
+                  value: 'yes_picture',
+                },
+                {
+                  title: '😞 Nope. What?',
+                  value: 'no',
+                },
+              ],
+            },
+            user: {
+              _id: 2,
+              name: 'React Native',
+              avatar: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1703743334,411607732&fm=26&gp=0.jpg',
+            },
+          },...messages])
+    },[messages])
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
             <View style={style.container}>
                 <Header title="小可爱" leftEvent={leftEvent}>
                 </Header>
+                <Button onPress={receiveMessage} title="receive"></Button>
                 <GiftedChat
                     messages={messages}
                     onSend={messages => onSend(messages)}
